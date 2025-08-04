ae7b824123e3bdedcce3987298bd8d51aca8d2b2
vincentastolfi <vincent.astolfi@etu.uca.fr>
2025-08-04 22:16:50 +0200

    feat: detect plok file

--- src/main.c
+++ src/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-  puts("Hello World!");
+  if (argc == 1) {
+    puts("[ERROR] - No plok file found, use it like so : ./plok example.pk"); 
+    return 1;
+  }
+
+  size_t fnl = strlen(argv[1]);
+  char extension[4];
+
+  if (fnl >= 4) {
+    strncpy(extension, argv[1] + fnl - 3, 3);
+    extension[3] = '\0';
+  } else {
+    puts("[ERROR] - Extension file not recognize, use  please plok file only");
+    return 1;
+  }
+
+  if (strcmp(extension, ".pk") != 0) {
+    puts("[ERROR] - Extension file not recognize, use  please plok file only");
+    return 1;
+  }
+
+  puts("[INFO] - Start lexer");
+
   return 0;
 }
